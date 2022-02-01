efedb548047b8493b3d29a42f7e48cc7a22dce02
Vinicius Gonçalves de Oliveira <vinicius@myriaddev.me>
2022-01-31 22:58:10 -0300

    fix: on_ready is async and self.status is initialized correctly

--- dezbot/bot.py
+++ dezbot/bot.py
@@ -6,16 +6,22 @@ class Dezbot(BotBase):
     Dezbot Discord Bot
     """
 
-    def __init__(self, token: str, version: str, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(
+        self, token: str, command_prefix: str, version: str, *args, **kwargs
+    ):
+        super().__init__(
+            status=f"{command_prefix}help",
+            command_prefix=command_prefix,
+            *args,
+            **kwargs,
+        )
         self.token = token
         self.version = version
-        self.status = f"{self.command_prefix}help"
 
     def run(self) -> None:
         super().run(self.token)
 
-    def on_ready(self):
+    async def on_ready(self):
         print(
-            f"Logged in as {self.user.name}#{self.user.discriminator}({self.version})"
+            f"Logged in as {self.user.name}#{self.user.discriminator}(v{self.version})"
         )
